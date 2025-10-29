68b107534ffa1fabd86d1c094f5a6083db173d2d
shashidhargangolli <58553112+shashidhargangolli@users.noreply.github.com>
2025-10-29 19:29:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
+  ami           = var.ami
+  instance_type = var.instance_type
 
- tags = {
-   Name = var.name_tag,
- }
+  tags = {
+    Name = var.name_tag,
+  }
 }
